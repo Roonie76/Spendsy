113965a7c564f7a4d2f57e9967ef270496cc441f
Roonie76 <rohinvengatesh04@gmail.com>
2026-03-28 01:02:20 +0530

    chore: remove legacy parser components and add missing init files

---
+++ spendsy/services/finance-service/app/utils/pdf/__init__.py
@@ -0,0 +1,15 @@
+from .extractor import (
+    PdfPageContent,
+    extract_pdf_pages,
+    extract_text_sample,
+    has_meaningful_text,
+    normalize_pdf_text,
+)
+
+__all__ = [
+    "PdfPageContent",
+    "extract_pdf_pages",
+    "extract_text_sample",
+    "has_meaningful_text",
+    "normalize_pdf_text",
+]
